50315f325d5c4e7a6f91957feffcd2b9aa95e6a6
Michal <steffan_17@wp.pl>
2021-07-05 14:07:15 +0200

    DB dev

--- public/dbInterface/js/db_front.js
+++ public/dbInterface/js/db_front.js
@@ -36,7 +36,7 @@ const querryForm = document.getElementById('sqlQuerryForm').addEventListener(
 const renderHTMLTableNames = function(array)
 {
     return `
-        <ul>
+        <ul id='tablesList' class='tablesList'>
         ${array.map((tableName)=>{
             return `
             <li class='tableName' name='${tableName}'>${tableName}</li>`
@@ -51,7 +51,6 @@ const showTables = function()
     fetch('/api/showTables').then(res => res.json()).then(data => {
         result.innerHTML=renderHTMLTableNames(data)
         const tableNameButton = document.getElementsByClassName('tableName')
-        console.log(tableNameButton)
         const tableNamesArray= [...tableNameButton].map((e)=>{e.addEventListener('click', ()=>{showTheTable(e.attributes.name.value)})})
     })
 }
@@ -60,7 +59,52 @@ showTables()
 
 const showTheTable = (tableName)=>{
 
-    console.log(tableName)
-    fetch(`/api/showTheTable?tableName=${tableName}`)
+    fetch(`/api/showTheTable?tableName=${tableName}`).then(res => res.json()).then(data => {
+        const existTableContent = document.getElementsByClassName('tableContent')
+        
+        if(existTableContent.length){
+            existTableContent.innerHTML='ale'
+            console.log(existTableContent)
+            }
+        const tableContent = document.createElement('div')
+        tableContent.className = 'tableContent';
+        tableContent.innerHTML=renderTableHTML(tableName, data)
+        result.appendChild(tableContent)
+        const showTableBack = document.getElementById('showTableBack').addEventListener('click', ()=>showTables())
+
+    })
 
 }
+
+const renderTableHTML = function(tableName, array)
+{
+   
+    
+    const tableNames = [] 
+    Object.keys(array[0]).map((e) => {tableNames.push(e)})
+    
+    return `
+    <div id=showTableBack><<<<<</div>
+    <table id='tableTable' class='tableTable'>
+    <caption>${tableName}</caption>
+   
+    <thead> 
+    ${tableNames.map(e =>{return `<th>${e}</th>`}).join('')}
+    </thead>
+    ${array.map(arrayMap => {
+        
+        return `<tr>
+            ${tableNames.map(tableNamesMap => {
+
+                return `<td>${arrayMap[tableNamesMap]}</td>`
+
+            }).join('')}
+        <td><div id=editButton><img src=../img/icoEdit.png width=20px></div></td>
+        <td><div id=editButton><img src=../img/icoDelete.png width=20px></div></td>
+        </tr>`
+
+    }).join('')}
+    </table>`
+    
+}
+
--- routes/actions.js
+++ routes/actions.js
@@ -1,4 +1,4 @@
-const db = require("../db/db")
+const db = require("../db/db_server")
 
 const theDatabase = './db/database'
 
